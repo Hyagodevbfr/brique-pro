4b25fae27cd853b102ce801ae67a93287883e3fb
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-25 10:04:58 -0300

    Add MoneyErrors class for standardized error handling

---
+++ BriquePro.Domain/Common/ErrorsHandling/ValueObjects/MoneyErrors.cs
@@ -0,0 +1,18 @@
+﻿namespace BriquePro.Domain.Common.ErrorsHandling.ValueObjects
+{
+    public static class MoneyErrors
+    {
+        public static readonly Error NegativeAmount = new(
+            "NegativeAmount",
+            "Value cannot be negative."
+        );
+        public static readonly Error TooManyDecimalPlaces = new(
+            "TooManyDecimalPlaces",
+            $"Money cannot have more than 4 decimal places."
+        );
+        public static readonly Error InvalidCurrency = new(
+            "InvalidCurrency",
+            "Currency must be a 3-letter ISO code."
+        );
+    }
+}
