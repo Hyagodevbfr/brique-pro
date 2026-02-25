2cc0ec6c83741435308904961c1c0c92b4e7a151
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-25 10:05:23 -0300

    Add Result<TValue> and enhance Result for error handling

---
+++ BriquePro.Domain/Common/ErrorsHandling/Result.cs
@@ -0,0 +1,55 @@
+﻿namespace BriquePro.Domain.Common.ErrorsHandling
+{
+    public class Result<TValue>
+    {
+        public bool IsSuccess { get; }
+        public TValue? Value { get; }
+        public object? Error { get; }
+
+        private Result(TValue value)
+        {
+            IsSuccess = true;
+            Value = value;
+            Error = null;
+        }
+
+        private Result(object error)
+        {
+            IsSuccess = false;
+            Value = default!;
+            Error = error;
+        }
+
+        public static Result<TValue> Success(TValue value) => new(value);
+
+        public static Result<TValue> Failure(Error error) => new(error);
+
+        public static Result<TValue> Failure(ValidationError validationError)
+            => new(validationError);
+
+        public static implicit operator Result<TValue>(TValue value) => Success(value);
+
+        public static implicit operator Result<TValue>(Error error) => Failure(error);
+    }
+
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public object? Error { get; }
+        private Result()
+        {
+            IsSuccess = true;
+            Error = null;
+        }
+        private Result(object error)
+        {
+            IsSuccess = false;
+            Error = error;
+        }
+        public static Result Success() => new();
+        public static Result Failure(Error error) => new(error);
+        public static Result Failure(ValidationError validationError)
+            => new(validationError);
+        public static implicit operator Result(Error error) => Failure(error);
+    }
+}
