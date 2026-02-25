bd22ae342a49de7f0828304a40841bd2d0eb1461
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-25 10:06:22 -0300

    Introduce BaseEntity class for shared entity behavior

---
+++ BriquePro.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,51 @@
+﻿using BriquePro.Domain.Common.ErrorsHandling;
+using Flunt.Notifications;
+
+namespace BriquePro.Domain.Entities
+{
+    public abstract class BaseEntity : Notifiable<Notification>
+    {
+        public long Id { get; protected set; }
+        public bool IsDeleted { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
+
+        protected BaseEntity()
+        {
+            CreatedAt = DateTime.UtcNow;
+            IsDeleted = false;
+        }
+        protected void MarkAsUpdated()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        protected Result MarkAsDeleted()
+        {
+            if (IsDeleted)
+                return Result.Failure(new Error("Entity.Deleted", "Entity is already deleted."));
+
+            IsDeleted = true;
+            MarkAsUpdated();
+
+            return Result.Success();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not BaseEntity other)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+    }
+}
