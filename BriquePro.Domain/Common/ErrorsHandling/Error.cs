6be446b4a859653c204a54b1b473657e48290e3c
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-25 10:04:04 -0300

    Add Error record for structured error representation

---
+++ BriquePro.Domain/Common/ErrorsHandling/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace BriquePro.Domain.Common.ErrorsHandling
+{
+    public record Error(string Code, string Description)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+    }
+}
