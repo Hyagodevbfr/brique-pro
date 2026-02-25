344c413f9b7584764243f2cf290e9be605c2b966
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-25 10:04:28 -0300

    feat: add validation Error class

---
+++ BriquePro.Domain/Common/ErrorsHandling/ValidationError.cs
@@ -0,0 +1,17 @@
+﻿using Flunt.Notifications;
+
+namespace BriquePro.Domain.Common.ErrorsHandling
+{
+    public sealed record ValidationError(IReadOnlyCollection<Error> Errors)
+    {
+        public static ValidationError FromNotifications(
+        IReadOnlyCollection<Notification> notifications)
+        {
+            var errors = notifications
+                .Select(n => new Error(n.Key, n.Message))
+                .ToList();
+
+            return new ValidationError(errors);
+        }
+    }
+}
