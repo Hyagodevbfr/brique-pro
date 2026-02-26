3c2d44c0e7aad7f320673ac506e47db22c5f1c96
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-26 08:17:23 -0300

    feat: create email address value object

---
+++ BriquePro.Domain/ValueObjects/EmailAddress.cs
@@ -0,0 +1,35 @@
+﻿using BriquePro.Domain.Common;
+using BriquePro.Domain.Common.ErrorsHandling;
+using System.ComponentModel.DataAnnotations;
+
+namespace BriquePro.Domain.ValueObjects
+{
+    public class EmailAddress : ValueObject
+    {
+        public readonly string Value;
+
+        private EmailAddress(string value)
+        {
+            Value = value;
+        }
+
+        public Result<EmailAddress> Create(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return Result<EmailAddress>.Failure(new Error("Invalid.Email", "Email cannot be null or whitespace."));
+
+            email = email.Trim();
+
+            if (!IsValid(email))
+                return Result<EmailAddress>.Failure(new Error("Invalid.Email", "Email format is invalid."));
+
+            return new EmailAddress(email);
+        }
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+        private static bool IsValid(string email) => !string.IsNullOrEmpty(email) && new EmailAddressAttribute().IsValid(email);
+
+    }
+}
