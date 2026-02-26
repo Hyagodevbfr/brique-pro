9fefed33c49b7fb7ac90c19e0f2e3fb7fd75fdb4
Hyagodevbfr <hyago.sbrito@gmail.com>
2026-02-26 08:19:57 -0300

    feat: create User Entity

---
+++ BriquePro.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using BriquePro.Domain.ValueObjects;
+
+namespace BriquePro.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
+        public EmailAddress Email { get; private set; }
+        public string PasswordHash { get; private set; } = string.Empty;
+        public bool IsPremium { get; private set; }
+        public DateTime PremiumUntil { get; private set; }
+
+        protected User() { }
+
+
+
+    }
+}
